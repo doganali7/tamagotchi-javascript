50a8b12e9a5e6e6214f4b81fa7a16c963ee52f10
Ali Dogan <alidogn98@gmail.com>
2022-10-14 02:02:06 +0300

    created character function

---
+++ tests/character.test.js
@@ -0,0 +1,7 @@
+const Character = require("../src/character");
+
+describe("constructor", () => {
+  it("returns an object", () => {
+    expect(new Character("Mametchi")).toBeInstanceOf(Object);
+  });
+});
