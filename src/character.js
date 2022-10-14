779ed3fae1ec1a209565af420e74f852060a5d3a
Ali Dogan <alidogn98@gmail.com>
2022-10-14 18:14:01 +0300

    increment age

--- src/character.js
+++ src/character.js
@@ -1,5 +1,9 @@
 function Character(name) {
   this.name = name;
+  this.age = 0;
+  this.growUp = function () {
+    this.age += 1;
+  };
 }
 
 module.exports = Character;
--- tests/character.test.js
+++ tests/character.test.js
@@ -9,4 +9,27 @@ describe("constructor", () => {
     const character = new Character("Mametchi");
     expect(character.name).toEqual("Mametchi");
   });
+
+  it("has a initial age of 0", () => {
+    const character = new Character("Mametchi");
+    expect(character.age).toEqual(0);
+  });
+});
+
+describe("growUp", () => {
+  it("increments the age by 1", () => {
+    const character = new Character("Mametchi");
+
+    character.growUp();
+
+    expect(character.age).toEqual(1);
+  });
+
+  it("increments the age by 1", () => {
+    const character = new Character("Mametchi");
+
+    character.growUp();
+
+    expect(character.age).toEqual(1);
+  });
 });
